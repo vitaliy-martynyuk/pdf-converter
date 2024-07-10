31d3aa2b2fdfd41a5515857f5b8f380f507634ae
vitaliy-martynyuk <84335906+vitaliy-martynyuk@users.noreply.github.com>
2024-07-10 15:02:55 +0300

    apply UI changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,7 @@ function App() {
   const { convertData } = useConvert();
   const { getItem, setItem, stringifyItem, parseItem } = useLocalStorage();
   const [pdfUrl, setPdfUrl] = useState("");
-  const inputRef = useRef<HTMLInputElement | null>(null);
-
-  console.log(pdfUrl);
+  const inputRef = useRef<HTMLTextAreaElement | null>(null);
 
   const convertToPdf = (inputValue: string | undefined) => {
     if (inputValue) {
@@ -38,9 +36,9 @@ function App() {
   };
 
   return (
-    <div>
-      <div className="underline text-center">PDF converter</div>
+    <div className="bg-slate-100 grow grid grid-rows-8 grid-cols-8 gap-0 max-h-screen">
       <form
+        className="col-start-4 col-end-9 row-start-1 row-end-3 rounded-md mt-2 mr-2 mb-1 ml-1 bg-white flex flex-col justify-between box-border"
         onSubmit={(e) => {
           e.preventDefault();
           if (inputRef.current?.value) {
@@ -49,37 +47,57 @@ function App() {
           }
         }}
       >
-        <input
-          type="text"
+        <textarea
           name="text"
-          className="border-solid rounded-md border-slate-300 border-2"
+          className="grow border-solid rounded-tr-md rounded-tl-md border-slate-300 border-2 border-b-0 resize-none outline-0 p-3"
           ref={inputRef}
           required
-        />
+          placeholder="Enter your text here..."
+        ></textarea>
         <button
           type="submit"
-          className="border-solid rounded-md border-slate-300 border-2"
+          className="bg-red-700 hover:bg-red-600 duration-200 rounded-br-md rounded-bl-md text-white border-solid border-red-700 border-2 border-t-0 p-1"
         >
           Convert
         </button>
       </form>
-      {getItem("items") && (
-        <ul>
-          {parseItem(getItem("items") as string).map((el: any) => (
-            <li key={el.id} onClick={() => convertToPdf(el.value)}>
-              {el.value}
-            </li>
-          ))}
-        </ul>
-      )}
-      {pdfUrl && (
-        <PDFViewer
-          document={{
-            url: pdfUrl,
-          }}
-          loader={<div>loading...</div>}
-        />
-      )}
+      <div className="col-start-1 col-end-4 row-start-1 row-end-9 border-solid rounded-md border-slate-300 border-2 mt-2 mr-1 mb-2 ml-2 bg-white p-3 overflow-y-auto">
+        {getItem("items") ? (
+          <>
+            <h2 className="text-lg font-bold">Recent conversions:</h2>
+            <ul className="list-square ml-4">
+              {parseItem(getItem("items") as string).map((el: any) => (
+                <li
+                  key={el.id}
+                  onClick={() => convertToPdf(el.value)}
+                  className="cursor-pointer hover:text-red-600 duration-200"
+                >
+                  <div className="text-ellipsis overflow-hidden text-nowrap">
+                    {el.value}
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          <h2 className="text-lg font-bold">No recent conversions.</h2>
+        )}
+      </div>
+      <div className="col-start-4 col-end-9 row-start-3 row-end-9 border-solid rounded-md border-slate-300 border-2 mt-1 mr-2 mb-2 ml-1 bg-white flex overflow-y-auto">
+        {pdfUrl ? (
+          <PDFViewer
+            document={{
+              url: pdfUrl,
+            }}
+            loader={<div>loading...</div>}
+            css="flex grow"
+            canvasCss="flex grow"
+            hideNavbar
+          />
+        ) : (
+          <h3 className="text-lg font-bold m-3">No document selected.</h3>
+        )}
+      </div>
     </div>
   );
 }
